2305b4b98ab73cdb9d85483189b9a2c973a63738
jonphamm <jonpham09@gmail.com>
2022-08-23 16:20:11 -0400

    Add emoji and java to code

--- java.js
+++ java.js
@@ -1,18 +1,80 @@
-function getComputerChoice() {
-    const choice = ['rock', 'paper', 'scissors'];
-    return choice(Math.floor(Math.random() * choice.length));
+/*
+let playerScore = 0;
+let computerScore = 0;
+
+
+function computerPlay() {
+    const pick = ['rock', 'paper', 'scissors'];
+    return pick[Math.floor(Math.random() * pick.length)];
+ }
+ 
+ function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+       return `It's a tie! you both picked ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        // playerScore = playerScore + 1, playerScore--, playerScore = playerScore - 1
+        playerScore++
+       return "You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
+       return "You win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
+       return "You win! Scissors beats Paper";
+    } else {
+        computerScore++
+       return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+ }
+ 
+function game() {
+    for(let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+    }
+   
+    if(playerScore > computerScore) {
+        return 'You beat the computer!';
+    } else if(playerScore < computerScore) {
+        return 'You lost to the computer!';
+    } else {
+        return 'You tied with the computer!';
+    }
+} */
+
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '🪨',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '📝',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✂️',
+        beats: 'paper'
+    }
+]
+
+selectionButtons.forEach(selectionButtons => {
+    selectionButtons.addEventListener('click', e => {
+        const selectionName = selectionButtons.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selectionName)
+    })
+})
+
+function makeSelection(selection) {
+    console.log(selection)
 }
 
-function roundPlay(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return (`It's a tie! You both picked ${playerSelection}`);
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You win! Rock beats Scissors";
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return ('You lose! Paper beats Rock');
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return ('You win! Scissors beats Paper');
-    } else {
-        return (`You lose! ${computerSelection} beats ${playerSelection}`);
-    }
-} 
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
